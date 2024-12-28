8f678f447214ee83a3a47624084ef40e80ec2537
kopalmuhamad <muhamadkopal32@gmail.com>
2024-12-28 16:55:21 +0700

    add list category to home page

--- src/views/home-view/index.tsx
+++ src/views/home-view/index.tsx
@@ -1,9 +1,42 @@
+import { buttonVariants } from "@/components/atoms/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/atoms/carousel";
 import Hero from "@/components/organisme/hero";
+import CategoryCard from "@/components/shared/category-card";
+import Container from "@/components/shared/container";
+import { useCategories } from "@/features/categories/hooks/use-categories";
+import { cn } from "@/lib/utils";
+import { Link } from "react-router-dom";
 
 const HomeView = () => {
+  const { data: categories } = useCategories();
+
   return (
     <div>
       <Hero />
+      <Container className="py-4 space-y-4">
+        <header className="flex items-center justify-between">
+          <h1 className="text-xl font-semibold">Category</h1>
+          <Link
+            to={"/categories"}
+            className={cn(buttonVariants({ variant: "link" }))}
+          >
+            View all
+          </Link>
+        </header>
+        <Carousel>
+          <CarouselContent>
+            {categories?.map((category) => (
+              <CarouselItem className="basis-1/1">
+                <CategoryCard key={category._id} category={category} />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </Container>
     </div>
   );
 };
